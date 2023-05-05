c123ca93e721f465e42e679cbb21edf9add253e4
Federico_Enriquez <fedenri98@gmail.com>
2023-05-05 18:46:13 -0300

    feat: movie searcher random challenge

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect, useRef, useCallback } from "react";
+import debounce from "just-debounce-it";
+
+import { Movies } from "./components/Movies";
+import { useMovies } from "./hooks/useMovies";
+
+import "./App.css";
+
+function useSearch() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+
+    if (search === "") {
+      setError("No se puede buscar una pelicula vacia");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 carácteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, updateSearch, error };
+}
+
+function App() {
+  const [sort, setSort] = useState(false);
+
+  const { search, updateSearch, error } = useSearch();
+  const { movies, getMovies, loading } = useMovies({ search, sort });
+
+  const debounceGetMovies = useCallback(
+    debounce((search) => getMovies({ search }), 300),
+    [getMovies]
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    getMovies({ search });
+  };
+
+  const handleSort = () => {
+    setSort(!sort);
+  };
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+
+    debounceGetMovies(newSearch);
+  };
+
+  return (
+    <div className="page">
+      <header>
+        <h1>Movie Searcher</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={search}
+            name="query"
+            placeholder="Avengers, Star Wars, The Matrix..."
+          />
+          <input type="checkbox" onChange={handleSort} checked={sort} />
+          <button type="submit">Search</button>
+        </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </header>
+
+      <main>{loading ? <p>Loading...</p> : <Movies movies={movies} />}</main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Movies.jsx
@@ -0,0 +1,23 @@
+function MoviesList({ movies }) {
+  return (
+    <ul className="movies">
+      {movies.map((movie) => (
+        <li className="movie" key={movie.id}>
+          <h3>{movie.title}</h3>
+          <p>{movie.year}</p>
+          <img src={movie.poster} alt={movie.title} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function NoMoviesResults() {
+  return <p>Movie not found!</p>;
+}
+
+export function Movies({ movies }) {
+  const hasMovies = movies?.length > 0;
+
+  return hasMovies ? <MoviesList movies={movies} /> : <NoMoviesResults />;
+}
